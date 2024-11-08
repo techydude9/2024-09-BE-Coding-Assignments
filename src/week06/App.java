81de07781ad960c00be74cb000e2135f41f7580d
techydude9 <bob.ruzga@gmail.com>
2024-11-08 10:37:06 -0500

    Week06 setup done ready to code game logic

--- src/week06/App.java
+++ src/week06/App.java
@@ -1,4 +1,7 @@
 package week06;
+
+import java.util.Scanner;
+
 /*
  * November 3 - 9, 2024 BE Coding Final
  * Author: Bob Ruzga
@@ -8,29 +11,46 @@ public class App {
 
 	public static void main(String[] args) {
 
+		Scanner userIn = new Scanner(System.in);
 		
-	// Add your code here to instantiate a Deck	
+	// Instantiate a Deck	
     	Deck cardDeck = new Deck();
     
-    // Call the describe method on the newly instantiated deck.
+    // Call the describe method on the newly instantiated deck to verify it created correctly.
     	cardDeck.describe();
-	
 	
 	// Deck shuffle() Method:
     	System.out.println("\nDeck shuffle() method");
     	cardDeck.shuffle();
 	
-	
-	// Call the describe method on the newly shuffled deck.
+	// Call the describe method to verify the deck shuffled.
 		cardDeck.describe();
 
-	// Testing Player
-		Player players = new Player("Bob");
-		players.describe();
+	// 	Create 2 Players by prompting for their names
+		System.out.println("\nFirst Player's name: ");
+		Player player1 = new Player(userIn.next());
+		player1.describe();
 		
+		System.out.println("\nSecond Player's name: ");
+		Player player2 = new Player(userIn.next());
+		player2.describe();
 		
+	// Let's deal the cards to the two new players
+		for (int i = 0; i < 52; i++) {
+			if (i % 2 == 0) {
+				player1.draw(cardDeck);
+			} else {
+				player2.draw(cardDeck);
+			}
+		}
 		
+		// Using for testing only
+		player1.describe();
+		player2.describe();
+		// remove after testing
 		
+		// close the Scanner
+		userIn.close();
 		
 	}  // end of main
 
--- src/week06/Player.java
+++ src/week06/Player.java
@@ -11,12 +11,13 @@ import java.util.List;
 public class Player {
 	String playerName;
 	int playerScore;
-	List<Card> playerHand = new ArrayList<>();
+	List<Card> playerHand;
 	
 	Player(String playerName) {
 		this.playerName = playerName;
 		this.playerScore = 0;
-	}
+		this.playerHand = new ArrayList<>();
+	}  // end of Player constructor
 
 	// describe method
 	public void describe() {
@@ -24,16 +25,11 @@ public class Player {
 		System.out.println("Player: " + getPlayerName());
 		System.out.println("\tHas a score of: " +getPlayerScore());
 		System.out.println("\t and the following cards:");
-		
-		// test adding a card
-		//playerHand.add.setName("Eight of Diamonds");
-		// playerHand[0].setValue(8);
-		
+			
 		int ctr = 0;
-		
 		for (Card aCard : playerHand) {
 			aCard.describe();
-			ctr =+ 1;
+			ctr += 1;
 		}
 		
 		System.out.println("\nTotal cards in hand: " + ctr);
@@ -67,9 +63,22 @@ public class Player {
 	public void setPlayerHand(List<Card> playerHand) {
 		this.playerHand = playerHand;
 	}
+	// End of getters and setters
 	
+	// incrementScore method
+	public void incrementScore() {
+		this.playerScore += 1;
+	}
+	
+	// draw cards for this player
+	public void draw(Deck shuffledDeck) {
+		this.playerHand.add(shuffledDeck.draw());
+	 }
 		
-		
+	// Flip a card for a players turn
+	public Card flip() {
+		return this.playerHand.remove(0);
+	}
 	
 	
 } // end of Player class
