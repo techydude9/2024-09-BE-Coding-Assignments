918673116de40d7274ddd440526f2627b512a810
techydude9 <bob.ruzga@gmail.com>
2025-01-17 15:52:58 -0500

    Week 13 project files thru video 4 completed

---
+++ week13-16/src/main/java/pet/park/PetParkApplication.java
@@ -0,0 +1,14 @@
+package pet.park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetParkApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PetParkApplication.class, args);
+		
+	} // end of main method
+
+} // end of PetParksApplication class
