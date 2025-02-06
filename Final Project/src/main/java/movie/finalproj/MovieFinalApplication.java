b4d77419126f6c71495e5e22630338a4282e003e
techydude9 <bob.ruzga@gmail.com>
2025-02-06 16:17:38 -0500

    Week 15 Vid3 Successfully completed for lab and final project

---
+++ Final Project/src/main/java/movie/finalproj/MovieFinalApplication.java
@@ -0,0 +1,13 @@
+package movie.finalproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieFinalApplication.class, args);
+	}  // end of main method -----
+
+}  // end of MovieFinalApplication class ----------
