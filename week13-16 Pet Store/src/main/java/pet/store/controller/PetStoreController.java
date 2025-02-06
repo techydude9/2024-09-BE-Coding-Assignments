65612a409319dcaa00bd24b8b9d7c6e079124e51
techydude9 <bob.ruzga@gmail.com>
2025-02-06 09:11:21 -0500

    Week 14 Coding assignment fully completed and successfully working

--- week13-16 Pet Store/src/main/java/pet/store/controller/PetStoreController.java
+++ week13-16 Pet Store/src/main/java/pet/store/controller/PetStoreController.java
@@ -2,7 +2,9 @@ package pet.store.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -27,6 +29,14 @@ public class PetStoreController {
 		return petStoreService.savePetStore(petStoreData);
 		
 	}  // end of insertPetStore method ---
+	
+	@PutMapping("/{petStoreId}")
+	public PetStoreData updatePetStore(@PathVariable Long petStoreId, 
+			@RequestBody PetStoreData petStoreData) {
+		petStoreData.setPetStoreId(petStoreId);
+		log.info("Updating pet store {}", petStoreData);
+		return petStoreService.savePetStore(petStoreData);
+	} // end of updatePetStore method ------
 	
 	
 } // end of PetStoreController class ----
